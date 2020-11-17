c042b567851ddabdc7fba04f43e0f6cab75a24f8
Vlatko Tasevski <vlatko.tasevskii@gmail.com>
2020-11-17 09:30:37 +0100

    initial commit

---
+++ TestingGit/Services/Test.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingGit.Services
+{
+    public class Test
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+
+        public void Complex()
+        {
+            var rand = new Random();
+            a = rand.Next(1, 10);
+            b = rand.Next(1, 10);
+            //b= a-2
+            if(a==1 && b==2)
+            {
+                a = rand.Next(1, 10);
+                b = rand.Next(1, 10);
+                if (a == 1 && b == 2)
+                {
+                    a = rand.Next(1, 10);
+                    b = rand.Next(1, 10);
+                    if (a == 1 && b == 2)
+                    {
+                        a = rand.Next(1, 10);
+                        b = rand.Next(1, 10);
+                        if (a == 1 && b == 2)
+                        {
+                            a = rand.Next(1, 10);
+                            b = rand.Next(1, 10);
+                            if (a == 1 && b == 2)
+                            {
+                                a = rand.Next(1, 10);
+                                b = rand.Next(1, 10);
+                                if (a == 1 && b == 2)
+                                {
+                                    a = rand.Next(1, 10);
+                                    b = rand.Next(1, 10);
+                                    if (a == 1 && b == 2)
+                                    {
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public void Complex2()
+        {
+            var rand = new Random();
+            b = rand.Next(1, 10);
+            a = rand.Next(1, 10);
+            //b= a-2
+            if (a == 1 && b == 2)
+            {
+                a = rand.Next(1, 10);
+                b = rand.Next(1, 10);
+                if (a == 1 && b == 2)
+                {
+                    a = rand.Next(1, 10);
+                    b = rand.Next(1, 10);
+                    if (a == 1 && b == 2)
+                    {
+                        a = rand.Next(1, 10);
+                        b = rand.Next(1, 10);
+                        if (a == 1 && b == 2)
+                        {
+                            a = rand.Next(1, 10);
+                            b = rand.Next(1, 10);
+                            if (a == 1 && b == 2)
+                            {
+                                a = rand.Next(1, 10);
+                                b = rand.Next(1, 10);
+                                if (a == 1 && b == 2)
+                                {
+                                    a = rand.Next(1, 10);
+                                    b = rand.Next(1, 10);
+                                    if (a == 1 && b == 2)
+                                    {
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
